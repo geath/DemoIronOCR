80e49188e6e2bbc1e718ea89fcf43c0bdbf192f2
GeAth <geath@gathbi.com>
2023-05-09 17:59:07 +0300

    C0 Initial_lap:main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DemoIronOcr;
+class Program
+{
+    static void Main(string[] args)
+    {
+         DirectoryInfo diTop = new DirectoryInfo("C:\1\testdata"); 
+
+        IronOcr.Installation.LicenseKey = "IRONOCR.GIORGOSATHANASOPOULOS.14744-54786B1C94-LDE42PVELBVNK-QVG6T2TF52KR-LHMBPEJRMJME-W3XKPMNVUSWL-J4BS3BXE63PW-JKDK2G-THX77GMB3RGJUA-DEPLOYMENT.TRIAL-BVN66F.TRIAL.EXPIRES.04.JUN.2023"; 
+        PerformOCR.IronOCRDemo.IronOCRMultiple(di.GetFiles().FirstOrDefault(), AppSettings.Splitted2OCRFilePath, args[1]);        
+        return;
+    }
+}
