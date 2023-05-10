9c72d4128e67501437e53e468bf56902d25c30dd
George Athanasopoulos <geath@live.com>
2023-05-10 08:24:34 +0300

    C2 Ready to Demo_srv:main

---
+++ AppSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoIronOcr
+{
+    public static class AppSettings
+    {
+        public static string AppDrive = @"D:";
+        public static string AppRootDirectoryName = $@"1";
+        public static string AppDataSourceDirectoryName = "testdata";
+        public static string AppDeliverablesDirectoryName = "Deliverables";
+        public static string AppDeliverablesTempDirectoryName = "temp";
+
+        public static string SourceDirectory = $@"{AppDrive}\{AppRootDirectoryName}\{AppDataSourceDirectoryName}";
+        public static string DeliverablesPath = $@"{AppDrive}\{AppRootDirectoryName}\{AppDeliverablesDirectoryName}";
+        public static string DeliverablesTempPath = $@"{DeliverablesPath}\{AppDeliverablesTempDirectoryName}";
+
+        public static string LogPath = $@"{DeliverablesPath}\logs";
+        public static string ErrorLogFilePath = Path.Combine(LogPath, $"errorLog_{DateTime.Now.ToString("yyyyMMdd")}.txt");
+        public static string LogFilePath = Path.Combine(LogPath, $"appLog_{DateTime.Now.ToString("yyyyMMdd")}.txt");
+        public static string AppTelemetryFilePath = Path.Combine(LogPath, $"appTelemetryLog_{DateTime.Now.ToString("yyyyMMdd")}.txt");
+
+        public static string ConvertedFiles2Tiff = Path.Combine(DeliverablesTempPath, "convertedFiles2Tif");
+        public static string SplittedFilesPath = Path.Combine(DeliverablesTempPath, "splitted");
+        public static string OCRCompletedFilePath = Path.Combine(DeliverablesPath, "OCRCompleted");
+        public static string OCRReadyFilePath = Path.Combine(DeliverablesTempPath, "OCRReady");
+        public static string PDFAFilesPath = Path.Combine(DeliverablesPath, "PDFACompleted");
+        public static string Splitted2OCRFilePath = Path.Combine(DeliverablesTempPath, "splitted2OCR");
+
+        public static string[] MainArgs1 = new string[] { "all", "cnvpdftif", "cnvjpgtif", "mergepdf", "splitfile", "ocr", "pdfa", "cnvocr", "splitocr", "ocrpdfa", "splitocrpdfa" };
+
+        public static int PdfSizeThreshold = 2097152;
+        public static int PdfPagingSize = 30;
+
+        public static string[] selectedFileTypes = new string[] { ".gif", ".jpg", ".tif", ".tiff", ".pdf", };        
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,10 +3,18 @@ class Program
 {
     static void Main(string[] args)
     {
-         DirectoryInfo diTop = new DirectoryInfo("C:\1\testdata"); 
+         DirectoryInfo diTop = new DirectoryInfo(@"D:\1\testdata"); 
+        foreach (var di in diTop.EnumerateDirectories("*")) 
+        {
 
-        IronOcr.Installation.LicenseKey = "IRONOCR.GIORGOSATHANASOPOULOS.14744-54786B1C94-LDE42PVELBVNK-QVG6T2TF52KR-LHMBPEJRMJME-W3XKPMNVUSWL-J4BS3BXE63PW-JKDK2G-THX77GMB3RGJUA-DEPLOYMENT.TRIAL-BVN66F.TRIAL.EXPIRES.04.JUN.2023"; 
-        PerformOCR.IronOCRDemo.IronOCRMultiple(di.GetFiles().FirstOrDefault(), AppSettings.Splitted2OCRFilePath, args[1]);        
-        return;
+            Console.Write($"Begin Process on Folder : {di.Name} \n");
+
+            // set directory for iterations
+            string setIteratedWorkingDirectory = di.FullName;
+
+            IronOcr.Installation.LicenseKey = "IRONOCR.GIORGOSATHANASOPOULOS.14744-54786B1C94-LDE42PVELBVNK-QVG6T2TF52KR-LHMBPEJRMJME-W3XKPMNVUSWL-J4BS3BXE63PW-JKDK2G-THX77GMB3RGJUA-DEPLOYMENT.TRIAL-BVN66F.TRIAL.EXPIRES.04.JUN.2023"; 
+            IronOCRDemo.IronOCRMultiple(di.GetFiles().FirstOrDefault(), AppSettings.OCRReadyFilePath);        
+            return;
+        }
     }
 }
